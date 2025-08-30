0e899b69bc0dd777ad3a355f5f43436eeef10820
Santi13 <s.penunuri@hotmail.com>
2025-08-30 13:31:48 -0600

    removed vision from bravo_amr

--- src/bravo_amr/BRAVO_AGV/vision/setup.sh
+++
@@ -1,3 +0,0 @@
-sudo bash ./vision/pyorbbecsdk/scripts/install_udev_rules.sh
-export PYTHONPATH=$PYTHONPATH:$(pwd)/vision/pyorbbecsdk/install/lib/
-sudo udevadm control --reload-rules && sudo udevadm trigger
