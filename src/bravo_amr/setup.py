1c4fce25f268e8b1be466d87a36efafcd7e79f58
RicardoBerumen <A01641180@tec.mx>
2025-08-30 12:09:37 -0600

    push and merge of bravo_agv repository

---
+++ src/bravo_amr/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'bravo_amr'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ricbear',
+    maintainer_email='ricberar@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'bravo_node = BRAVO_AGV.bravo_node:main',
+            'orbbec_det = BRAVO_AGV.vision.scripts.ros_color_depth:main',
+            'robot_control = BRAVO_AGV.navigation.scripts.robot_kinematics_ros:main',
+            'teleop_keyboard = BRAVO_AGV.navigation.scripts.agv_teleop:main',
+            'obj_control = BRAVO_AGV.navigation.scripts.objective_control:main',
+        ],
+    },
+)
